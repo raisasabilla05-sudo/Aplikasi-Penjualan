ef8eceb3d52a3484c2b2acef8022be52dce9ed47
raisasabilla05-sudo <raisasabilla05@gmail.com>
2026-07-06 11:40:51 +0700

    Upload proyek

---
+++ src/tampilan/UserID.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tampilan;
+
+/**
+ *
+ * @author raisa
+ */
+public class UserID {
+    private static String id;
+
+    public static void setUserLogin(String kode){
+        id = kode;
+    }
+
+    public static String getUserLogin(){
+        return id;
+    }
+}
